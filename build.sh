2f39f7530cf1cb4489666d95a307de6974818b03
varg247 <varg247@gmail.com>
2023-04-21 21:51:34 +0800

    前端依赖变更

--- build.sh
+++ build.sh
@@ -34,7 +34,7 @@ FetchWebDev() {
 }
 
 FetchWebRelease() {
-  curl -L https://github.com/alist-org/alist-web/releases/latest/download/dist.tar.gz -o dist.tar.gz
+  curl -L https://github.com/varg1714/alist-web/releases/latest/download/dist.tar.gz -o dist.tar.gz
   tar -zxvf dist.tar.gz
   rm -rf public/dist
   mv -f dist public
